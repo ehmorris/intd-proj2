dafd6f627e06b1fed3376d240fa9591635e44407
Edwin <edwin.h.morris@gmail.com>
2012-10-17 11:17:04 -0400

    added starfield

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+$(function() {
 
+  start();
+
+  $(window).resize(resize);
+
+  $(document).mousedown(function() {
+    context.fillStyle='rgba(0,0,0,'+opacity+')';
+  });
+
+  $(document).mouseup(function() {
+    context.fillStyle='rgb(0,0,0)';
+  });
+});
