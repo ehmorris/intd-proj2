86ba6458155efd0530ecff2c525e6bb36366341e
Edwin <edwin.h.morris@gmail.com>
2012-11-14 00:19:07 -0500

    add red overlay and popup info

--- app.js
+++ app.js
@@ -5,7 +5,21 @@ $(function() {
   $(window).resize(resize);
 
   $('.speedometer li').click(function() {
+    // toggle nav classes
+    $('.speedometer li').removeClass('active');
+    $(this).addClass('active');
+
+    // toggle modal classes
+    $('aside').hide();
+    var this_id = $(this).data('modal');
+    $('aside[data-modal='+this_id+']').show();
+
+    // set star speed
     var index = $(this).index() + 1;
     set_speed(index * 4);
+
+    // set overlay color
+    $('.overlay').css('opacity', star_speed * .01);
   });
+
 });
